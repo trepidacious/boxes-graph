61079787c6e6c883ba9d9ff3271ea4e3c4e5cd8b
trepidacious <trepidacious@gmail.com>
2016-03-20 10:43:41 +0000

    Refactors for possible speed improvements
    Create a "compatible" buffered image rather than just a plain one.
    Cache AffineTransform.
    Don't bother transforming when scaling is 1.0.
    Use Image rather than BufferedImage, so we could potentially use
    VolatileImage in future (doesn't really seem to work well though).

--- src/main/scala/org/rebeam/boxes/graph/swing/GraphBuffer.scala
+++ src/main/scala/org/rebeam/boxes/graph/swing/GraphBuffer.scala
@@ -3,51 +3,72 @@ package org.rebeam.boxes.graph.swing
 import org.rebeam.boxes.graph._
 import org.rebeam.boxes.swing._
 
-import java.awt.{Graphics, Graphics2D, Color, RenderingHints, BasicStroke, Image, AlphaComposite}
+import java.awt.{Graphics, Graphics2D, Color, RenderingHints, BasicStroke, Image, AlphaComposite, GraphicsEnvironment, Transparency}
 import java.awt.geom.{AffineTransform, Path2D, PathIterator, Rectangle2D}
 import java.awt.image.{BufferedImage}
 
 object GraphBuffer {
-  val clearComposite = AlphaComposite.getInstance(AlphaComposite.CLEAR, 0.0f)  
+  // val clearComposite = AlphaComposite.getInstance(AlphaComposite.CLEAR, 0.0f)  
 }
 
 class GraphBuffer() {
+  
+  private var renderSize: Vec2 = Vec2(10, 10)
+  private var displaySize: Vec2 = Vec2(10, 10)
 
-  private var renderImage: BufferedImage = new BufferedImage(10, 10, BufferedImage.TYPE_INT_ARGB)
-  private var displayImage: BufferedImage = new BufferedImage(10, 10, BufferedImage.TYPE_INT_ARGB)
+  private var renderImage: Image = newImage(renderSize)
+  private var displayImage: Image = newImage(displaySize)
+  
   private var scaling = 1.0
+  private var transform = AffineTransform.getScaleInstance(1.0, 1.0)
 
   val swapLock = new Object()
   
+  private def newImage(size: Vec2) = {
+    val w = math.max(size.x.asInstanceOf[Int], 1)
+    val h = math.max(size.y.asInstanceOf[Int], 1)
+    val env = GraphicsEnvironment.getLocalGraphicsEnvironment()
+    val device = env.getDefaultScreenDevice()
+    val config = device.getDefaultConfiguration()
+    config.createCompatibleImage(w, h, Transparency.TRANSLUCENT)
+    //Was: new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
+  }
+  
   def ensureRenderSize(area: Vec2): Unit = swapLock.synchronized {
-    val w = math.max(area.x.asInstanceOf[Int], 1)
-    val h = math.max(area.y.asInstanceOf[Int], 1)
-
-    if (renderImage.getWidth != w || renderImage.getHeight != h) {
-      renderImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
+    if (renderSize != area) {
+      println("Making new image, was " + renderSize + ", need " + area + ".")
+      renderImage = newImage(area)
+      renderSize = area
     }
   }
   
-  def clearRenderImage(): Unit = swapLock.synchronized {
-    val g = renderImage.getGraphics.asInstanceOf[Graphics2D]
-    g.setComposite(GraphBuffer.clearComposite)
-    g.fill(new Rectangle2D.Double(0,0,renderImage.getWidth, renderImage.getHeight))
-    g.dispose()
+  def display(gr: Graphics): Unit = {
+    swapLock.synchronized {
+      if (scaling == 1.0) {
+        gr.asInstanceOf[Graphics2D].drawImage(displayImage, 0, 0, null)      
+      } else {
+        gr.asInstanceOf[Graphics2D].drawImage(displayImage, transform, null)
+      }
+      val t3 = System.nanoTime()
+    }
   }
 
-  def display(gr: Graphics): Unit = swapLock.synchronized {
-    val s = 1.0/scaling
-    gr.asInstanceOf[Graphics2D].drawImage(displayImage, AffineTransform.getScaleInstance(s, s), null)
-  }
-
-  def imageToRender(): BufferedImage = swapLock.synchronized { 
+  def imageToRender(): Image = swapLock.synchronized { 
     renderImage 
   }
 
   def swap(newScaling: Double): Unit = swapLock.synchronized {
-    scaling = newScaling
+    if (scaling != newScaling) {
+      scaling = newScaling
+      val s = 1.0/scaling
+      transform = AffineTransform.getScaleInstance(s, s)
+    }    
     val i = displayImage
+    val s = displaySize
     displayImage = renderImage
+    displaySize = renderSize
     renderImage = i
+    
+    renderSize = s
   }
 }
